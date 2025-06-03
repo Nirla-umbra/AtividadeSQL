0201cc853d219140b46c92dc11dffa6bc8c8e8ec
Nirla <nirlapaz23@gmail.com>
2025-06-02 21:39:34 -0300

    Update and rename atividade.sql to SELECT names.sql

---
+++ SELECT names.sql
@@ -0,0 +1,56 @@
+SELECT name FROM world 
+ WHERE DOCK name LIKE '"Kia 
+
+SELECT name FROM world 
+ WHERE name LIKE 'Rook 
+
+ SELECT name FROR world 
+ WHERE name LIKE 
+ 
+ SELECT name FROM world 
+ HERE name LIKE ORDER BY 
+  
+SELECT name FROM world 
+ WHERE name LIKE
+
+SELECT name FROM world 
+WHERE LENGTH(name); 
+
+SELECT name FROM world 
+ WHERE capital; 
+
+SELECT name world 
+WHERE capital CONCAT(name, City') 
+
+SELECT capital, name FROM world 
+WHERE capital ATTE CONCAT('S", name, 
+
+SELECT capital, name FROM world 
+WERE capital THE CONCAT(name, s) di capital - name 
+
+  SELECT name FROM world 
+WHERE LOVE SELECT name FROM world 
+
+WHERE name LIKE 'Kland" 
+SELECT name FROM world 
+
+WHERE LIKE Case LIKE "Ria 
+ SELECT name FROM world 
+
+WHERE A LOVE Sook 
+SELECT name FROM world 
+ 
+WHERE name LIKE 
+SELECT name FROM world 
+
+WHERE name LIKE 
+ORDER MY SELECT name FROM world 
+
+SELECT name FROM world 
+WHERE LENGTH(name)-
+
+SELECT name FROM world 
+ WHERE name capital, 
+
+SELECT name FROM world 
+ WHERE capital CONCAT(name, City') 
